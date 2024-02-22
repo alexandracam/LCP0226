f86e162ca2e887428dc3a51155c9cda9a009335e
Alexandra Camarena <alexandracamarena@ou.edu>
2024-02-21 22:13:21 -0600

    Add project files.

---
+++ LCP0226/Program.cs
@@ -0,0 +1,22 @@
+﻿// MIS 3013 001
+// Feb 26, 2024
+// Alexandra Camarena
+// 113568154
+
+// define a function: function is hard topic
+double addTwoDouble(double d1, double d2) // double here means this function will produce a double
+{
+    double r;
+    r = d1 + d2;
+    return r;// this is the final product of this function
+}
+
+Console.WriteLine("Functions");
+
+int age1;
+Convert.ToInt32("20");// function call
+
+double r1;
+r1 = addTwoDouble(2.3,6.0);
+
+Console.WriteLine(r1);
